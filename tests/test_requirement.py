db09cbc9fbdff998cbc87694f0972cdf12dc29f0
sathsararns <sathsararns@gmail.com>
2026-08-05 13:57:03 +0530

    Comment out requirements agent test code for future reference

--- tests/test_requirement.py
+++ tests/test_requirement.py
@@ -1,14 +1,14 @@
-from app.agents.requirements_agent import requirements_agent
+# from app.agents.requirements_agent import requirements_agent
 
-response = requirements_agent.invoke(
-{
-"input":"I need a wedding photographer in Galle on August 15 at 10 AM"
-}
-)
+# response = requirements_agent.invoke(
+# {
+# "input":"I need a wedding photographer in Galle on August 15 at 10 AM"
+# }
+# )
 
-print(response)
+# print(response)
 
-print(response.service)
-print(response.location)
-print(response.date)
-print(response.time)
+# print(response.service)
+# print(response.location)
+# print(response.date)
+# print(response.time)
